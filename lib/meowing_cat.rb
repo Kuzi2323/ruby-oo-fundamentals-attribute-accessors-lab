01ca1a74982704589e5747d151c2def73110c37f
Kuzi2323 <kuzi2323@gmail.com>
2021-01-11 18:14:17 -0800

    commit tree 6ae7e25e579957f081b1a2cbd91459e497eed127

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,10 @@
-## code your solution here. 
+class Cat 
+    attr_accessor :name
+    define_method("meow") do 
+        puts "meow!"
+    end
+end
+
+maru = Cat.new
+maru.name = "Maru"
+maru.meow
